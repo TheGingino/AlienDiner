05e01cddd1db6748d0185e5b0813c4cf9f2360e5
Nikki van Wijngaarden <nikki.vw666@gmail.com>
2026-02-11 23:59:57 +0100

    Code Clean up

--- AlienDinerDash/Assets/Scripts/Character.cs
+++ AlienDinerDash/Assets/Scripts/Character.cs
@@ -7,20 +7,13 @@ using UnityEngine.AI;
 public class Character : MonoBehaviour
 {
     private NavMeshAgent _agent;
-
-    [Header("Movement Settings")] public float moveSpeed = 10f;
-
-    [Header("input setting")] 
-    [SerializeField] private float sampleDistance = 0.5f; //distance to point clicked
+    
     [SerializeField] private LayerMask groundLayer;
-
-    public static event System.Action<Vector3> OnGroundTouch;
+    
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-
-        _agent.speed = moveSpeed;
     }
 
     private void Update()
@@ -32,14 +25,10 @@ public class Character : MonoBehaviour
 
             if (Physics.Raycast(_ray, out RaycastHit hit, groundLayer))
             {
-                if (NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, sampleDistance, NavMesh.AllAreas))
+                if (NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, 100f, NavMesh.AllAreas))
                 {
                     _agent.SetDestination(navMeshHit.position);
-                                    
-                    OnGroundTouch?.Invoke(navMeshHit.position);
                 }
-                else
-                    Debug.Log("clicked point is not an walkable area.");
             }
         }
     }
