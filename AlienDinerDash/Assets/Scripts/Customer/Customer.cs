fe2446f084d7cbcdba58132df8c8974a8f138ddd
Gino Schaap <35615@ma-web.nl>
2026-02-16 15:22:40 +0100

    Made Changes to the Customer behavior
    Added difference in behavior where one type would leave faster than the other

--- AlienDinerDash/Assets/Scripts/Customer/Customer.cs
+++ AlienDinerDash/Assets/Scripts/Customer/Customer.cs
@@ -1,89 +1,159 @@
 using System;
 using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Customer : MonoBehaviour
 {
-    [Header("Customer Settings")]
-    [SerializeField] private CustomerSO customerSO;
-    private enum CustomerStates {HUNGRY, SERVED, FINISHED};
-    private CustomerStates currentState = CustomerStates.HUNGRY;
-    
+    [Header("Customer Settings")] [SerializeField]
+    private CustomerSO customerSO;
 
-    [Header("Waypoint to leave")]
-    private WaypointToLeave _waypointToLeave;
+    private enum CustomerStates
+    {
+        HUNGRY,
+        SERVED,
+        LEAVING
+    };
+
+    private CustomerStates currentState = CustomerStates.HUNGRY;
+
+    private bool _hasCoroutineStarted = false;
+    [SerializeField] private bool hasBeenServed = false;
+    //[SerializeField] private int itemsOrdered = 0;
+
+    [Header("Waypoint to leave")] private WaypointToLeave _waypointToLeave;
     private int nextWaypointIndex;
-    
+
     [SerializeField] private float speed = 5f;
     [SerializeField] private float reachDistance = 0.1f;
-    
-    [Header("Visual Testing")]
-    [SerializeField] private Slider customerTimerSlider;
-    [SerializeField] private Slider sliderTime;
-    
-    
-    
+
+    [Header("Visual Testing")] [SerializeField]
+    private Slider customerTimerSlider;
+
+    private float _sliderTime;
+
+
     private void Start()
     {
         _waypointToLeave = FindObjectOfType<WaypointToLeave>();
         customerTimerSlider = FindObjectOfType<Slider>();
-        
-        customerTimerSlider.maxValue = customerSO.customerFoodTimer;
+
+        customerTimerSlider.value = customerSO.customerFoodTimer;
     }
 
     private void Update()
     {
-        StartCoroutine(CustomerState());
-        Debug.Log("Customer is waiting");
+        if (currentState == CustomerStates.HUNGRY && !_hasCoroutineStarted)
+        {
+            StartCoroutine(CustomerState());
+            _hasCoroutineStarted = true;
+        }
     }
 
     private IEnumerator CustomerState()
     {
-        currentState = CustomerStates.HUNGRY;
-        Debug.Log("Customer is hungry");
-        
-        yield return new WaitForSeconds(customerSO.customerFoodTimer);
-        
-        currentState = CustomerStates.SERVED;
-        Debug.Log("Customer is served");
-        DecreaseSliderValue();
-        
-        yield return new WaitForSeconds(customerSO.customerFoodTimer);
-        
-        currentState = CustomerStates.FINISHED;
-        Debug.Log("Customer is finished");
-        LeaveRestaurant();
+        float waitTime = customerSO.customerTimer;
+        Debug.Log(waitTime);
+        switch (customerSO.customerType)
+        {
+            case CustomerType.ANNOYING:
+                yield return StartCoroutine(AnnoyingCustomer(waitTime));
+                Debug.Log("Annoying customer finished waiting." + waitTime);
+                break;
+            case CustomerType.AVERAGE:
+                yield return StartCoroutine(NormalCustomer(waitTime));
+                Debug.Log("Average customer finished waiting." + waitTime);
+                break;
+            case CustomerType.PATIENT:
+                yield return StartCoroutine(PatientCustomer(waitTime));
+                Debug.Log("Patient customer finished waiting." + waitTime);
+                break;
+        }
+    }
+
+    
+    private IEnumerator AnnoyingCustomer(float waitTime)
+    {
+        yield return StartCoroutine(CustomerBehavior(waitTime));
+    }
+    private IEnumerator PatientCustomer(float waitTime)
+    {
+        yield return StartCoroutine(CustomerBehavior(waitTime));
+    }
+    private IEnumerator NormalCustomer(float waitTime)
+    {
+        yield return StartCoroutine(CustomerBehavior(waitTime));
     }
     
+    private IEnumerator CustomerBehavior(float waitTime)
+    {
+        while (waitTime > 0)
+        {
+            DecreaseSliderValue();
+            if (hasBeenServed)
+            {
+                currentState = CustomerStates.SERVED;
+                Debug.Log("Customer received food!");
+                break;
+            }
+            waitTime -= Time.deltaTime;
+            yield return null;
+        }
+
+        CustomerType customerType = CustomerType.ANNOYING;
+        
+        if (!hasBeenServed && customerTimerSlider.value <= 0 && customerType == CustomerType.ANNOYING)
+        {
+            Debug.Log("Customer got tired of waiting and left!");
+            currentState = CustomerStates.LEAVING;
+            LeaveRestaurant();
+        }
+
+    }
+
     public void ServeFood()
     {
         if (currentState == CustomerStates.HUNGRY)
         {
+            hasBeenServed = true;
             currentState = CustomerStates.SERVED;
             Debug.Log("Customer received food!");
         }
     }
-    
+
     [ContextMenu("Testing the ability to leave the restaurant")]
     private void LeaveRestaurant()
-    { 
+    {
         Debug.Log("Customer is leaving");
-        transform.position = Vector3.MoveTowards(transform.position, _waypointToLeave.waypointToLeave[nextWaypointIndex].transform.position, Time.deltaTime * speed);
-        if (Vector3.Distance(transform.position, _waypointToLeave.waypointToLeave[nextWaypointIndex].transform.position) <= reachDistance)
+        StartCoroutine(MoveToExit());
+    }
+
+    private IEnumerator MoveToExit()
+    {
+        while (nextWaypointIndex < _waypointToLeave.waypointToLeave.Length)
         {
-            nextWaypointIndex += 1;
+            transform.position = Vector3.MoveTowards(transform.position,
+                _waypointToLeave.waypointToLeave[nextWaypointIndex].transform.position,
+                Time.deltaTime * speed);
+
+            if (Vector3.Distance(transform.position,
+                    _waypointToLeave.waypointToLeave[nextWaypointIndex].transform.position) <= reachDistance)
+            {
+                nextWaypointIndex += 1;
+            }
+
+            yield return null;
         }
         Destroy(gameObject);
     }
-    
+
     private void DecreaseSliderValue()
     {
-        customerTimerSlider.value = customerSO.customerFoodTimer;
-        if (customerTimerSlider.value > 0) customerTimerSlider.value -= Time.deltaTime;
-        customerTimerSlider.value = customerSO.customerFoodTimer;
-
-        Debug.Log("Customer timer: " + customerTimerSlider.value);
-      
+        if (customerTimerSlider.value >= 0) 
+        {
+            customerTimerSlider.value -= Time.deltaTime;
+        }
+        //Debug.Log("Customer timer: " + customerTimerSlider.value);
     }
 }
